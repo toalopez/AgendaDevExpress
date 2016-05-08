75fa1b1e990738ee0f6beede4959d619f7375f4e
Angel <lopez_angel@hotmail.com>
2016-05-08 14:38:51 -0400

    Basic DevExpress Template Schedule

---
+++ Agenda/MainAgenda.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using DevExpress.Skins;
+using DevExpress.LookAndFeel;
+using DevExpress.UserSkins;
+using DevExpress.XtraBars;
+using DevExpress.XtraBars.Ribbon;
+using DevExpress.XtraBars.Helpers;
+
+
+namespace Agenda
+{
+    public partial class MainAgenda : RibbonForm
+    {
+        public MainAgenda()
+        {
+            InitializeComponent();
+            InitSkinGallery();
+            schedulerControl.Start = System.DateTime.Now;
+        }
+        void InitSkinGallery()
+        {
+            SkinHelper.InitSkinGallery(rgbiSkins, true);
+        }
+
+        private void dateNavigator_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
